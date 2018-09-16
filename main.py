50414ed9b4495831ba8196883bfa890f9d87094b
David Verdejo <32431931+daveresbk@users.noreply.github.com>
2018-09-16 19:58:45 +0200

    Create main.py
    Initial version

---
+++ main.py
@@ -0,0 +1,36 @@
+import pycurl
+from flask import jsonify
+from io import BytesIO
+
+def request_pycurl(request):
+    """Http monitor.
+    Args:
+        page: valid url.
+    Returns:
+        Response code and timing
+    """
+    request_json = request.get_json()
+    if request.args and 'page' in request.args:
+        page = request.args.get('page')
+    elif request_json and 'page' in request_json:
+        page = request_json['page']
+    else:
+        return "Invalid parameters!",500
+    
+    result = ""
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, page)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    
+    data = {}
+    data['response_code'] = c.getinfo(c.RESPONSE_CODE)
+    data['namelookup'] = c.getinfo(c.NAMELOOKUP_TIME)
+    data['connect'] = c.getinfo(c.CONNECT_TIME)
+    data['starttransfer'] = c.getinfo(c.STARTTRANSFER_TIME)
+    data['total_time'] =  c.getinfo(c.TOTAL_TIME)
+    
+    c.close()
+    
+    return jsonify(data)
